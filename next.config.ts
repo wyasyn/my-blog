00d8ec16ac0c7cd2715a72515a8499401b832ed8
Yasin Walum <ywalum@gmail.com>
2025-03-10 02:47:53 +0300

    ignore ts errors

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,13 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
